c19f331550066a03c25177c2df2beaed5a8a5697
Grant Pierce <glpierce174@gmail.com>
2021-11-12 15:52:43 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const body = document.querySelector("body");
+const newHeader = document.createElement("h1");
+newHeader.textContent = "victory";
+newHeader.id = "victory";
+newHeader.innerHTML = "Grant is the champion";
+body.append(newHeader);
+
